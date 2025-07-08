d155bffb95a1a1f0f2c9cd0833d19a7027fc7b79
pratap9155 <venompratap22@gmail.com>
2025-07-08 16:38:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/IstkharXrobot/NhiPta",
+    "https://github.com/pratap9155/Mahi",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
